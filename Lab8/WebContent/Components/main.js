43fde4368eb725f8c120c94a1357ffad7f17d2f4
thami <thami@DESKTOP-LQJ1807>
2020-04-26 19:55:50 +0530

    Client-model: Generate the student card

--- Lab8/WebContent/Components/main.js
+++ Lab8/WebContent/Components/main.js
@@ -57,3 +57,41 @@ function validateItemForm() {
 	}
 	return true;
 }
+
+function getStudentCard(name, gender, year) {
+	
+	var title = (gender == "Male") ? "Mr." : "Ms."; 
+	var yearNumber = ""; 
+	
+	switch (year) {
+	case "1":
+		yearNumber = "1st"; 
+		break;
+	case "2":
+		yearNumber = "2nd"; 
+		break;
+	case "3":
+		yearNumber = "3rd"; 
+		break;
+	case "4":
+		yearNumber = "4th"; 
+		break;
+
+	default:
+		break;
+	}
+	
+	var student = ""; 
+	
+	student += "<div class=\"student card bg-light m-2\" style=\"max-width: 10rem; float: left;\">";
+	student += "<div class=\"card-body\">"; 
+	student += title + " " + name + ",";
+	student += "<br>"; 
+	student += yearNumber + " year";
+	student += "</div>"; 
+	student += "<input type=\"button\" value=\"Remove\" class=\"btn btn-danger remove\">";
+	student += "</div>"; 
+	
+	return student;
+}
+
