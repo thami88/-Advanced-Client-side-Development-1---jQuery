9bb5ce5ec5a85b39807ebb2a3f273fefe10e2d8f
thami <thami@DESKTOP-LQJ1807>
2020-04-26 19:32:28 +0530

    The Complete Save Button Handler

--- Lab8/WebContent/Components/main.js
+++ Lab8/WebContent/Components/main.js
@@ -3,3 +3,32 @@ $(document).ready(function()
  $("#alertSuccess").hide();
  $("#alertError").hide();
 }); 
+
+//SAVE ============================================
+$(document).on("click", "#btnSave", function(event)
+{
+// Clear status msges-------------
+ $("#alertSuccess").text("");
+ $("#alertSuccess").hide();
+ $("#alertError").text("");
+ $("#alertError").hide();
+// Form validation----------------
+var status = validateItemForm();
+// If not valid-------------------
+if (status != true)
+ {
+ $("#alertError").text(status);
+ $("#alertError").show();
+ return;
+ }
+// If valid-----------------------
+var student = getStudentCard($("#txtName").val().trim(),
+ $('input[name="rdoGender"]:checked').val(),
+ $("#ddlYear").val());
+ $("#colStudents").append(student);
+
+ $("#alertSuccess").text("Saved successfully.");
+ $("#alertSuccess").show();
+
+ $("#formStudent")[0].reset();
+});
